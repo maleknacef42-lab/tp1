4123f584a4ba92457424bbca443478ad2526faec
Malek Nacef <malek.nacef@etu.univ-lehavre.fr>
2026-04-07 09:42:01 +0200

    modif de readme

---
+++ Cryptomonnaie.java
@@ -0,0 +1,12 @@
+public class Cryptomonnaie 
+{
+    private String nom;
+    private double valeurDeJeton; // Imaginons en euros
+
+    public Cryptomonnaie(String nom, double valeurDeJeton) 
+    {
+        this.nom = nom;
+        this.valeurDeJeton = valeurDeJeton;
+    }
+}
+
